dfa45a4cc2d4bd656dd20598a721aec27a072359
yamasita-syuugo <100878782+yamasita-syuugo@users.noreply.github.com>
2024-03-30 13:42:16 +0900

    Add files via upload

---
+++ DirectX12_24_02_19/DirectX12_24_02_19/UseHeader.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include<vector>
+using namespace std;
+#include<wrl/client.h>
+using namespace Microsoft::WRL;
+
+#include<d3d12.h>
+#include<dxgi1_6.h>
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+#include<DirectXMath.h>
+using namespace DirectX;
+#include<d3dcompiler.h>
+#pragma comment(lib,"d3dcompiler.lib")
+#include<d3dx12.h>
+
+#include<DirectXTex.h>
+#pragma comment(lib,"DirectXTex.lib")
---
+++ DirectX12_24_02_19/DirectX12_24_02_19/value.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define WINDOW_LEFT (30)
+#define WINDOW_TOP (30)
+#define WINDOW_WIDTH (1280)
+#define WINDOW_HEIGHT (720)
+
